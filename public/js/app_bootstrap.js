89c21752b8b0f11dd3972c51c9d9c90258534a46
drivativ <drivativ@gmail.com>
2013-01-04 13:41:00 -0800

    fix tpl path

--- public/js/app_bootstrap.js
+++ public/js/app_bootstrap.js
@@ -3,8 +3,8 @@
  */
 
 DRIVATIV.utils.loadTemplate([
-	{name:'HeaderView', path:'/tpl', ext:'html'},
-	{name:'FooterView', path:'/tpl', ext:'html'}
+	{name:'HeaderView', path:'tpl', ext:'html'},
+	{name:'FooterView', path:'tpl', ext:'html'}
 	], function() {
     	CADENT.app = new CADENT.App();
 		CADENT.appView = new CADENT.AppView();
