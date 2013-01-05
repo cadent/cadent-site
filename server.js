ed847f94c0876667ca42dcfc5aa3d24f4c1ec7d0
drivativ <drivativ@gmail.com>
2013-01-05 11:12:04 -0800

    logging

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.configure(function () {
 });
 
 app.get('/projects', project.findAll);
-
+console.log("Server hit - " + app.get('port'));
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
