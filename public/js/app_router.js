b8f34a077ccbed55a9958e158e9fd076b8bc3764
drivativ <drivativ@gmail.com>
2013-01-11 11:07:39 -0800

    adding edit state

--- public/js/app_router.js
+++ public/js/app_router.js
@@ -13,11 +13,46 @@ CADENT.ApiVersion = 'v1';
 CADENT.App = Backbone.Router.extend({
     routes: {
         "": "setupApp",
-        "projects": "projects"
+        "edit": "edit"
     },
     
     setupApp: function () {
         console.log('ROUTER :: setupApp');
+        
+        this.headerView = new CADENT.HeaderView();
+        $('.header').html(this.headerView.el);
+        
+        $('.content').append('<br><br>');
+        
+        this.footerView = new CADENT.FooterView();
+        $('.footer').html(this.footerView.el);
+        /*
+        var proj = new CADENT.Project();
+        this.adminView = new CADENT.AdminView({model:proj});
+		$('.content').html(this.adminView.el);
+        
+        $('.content').append('<br><br>');
+        
+        var tagEdit = new CADENT.Tag();
+        this.tagEditView = new CADENT.TagEditView({model:tagEdit});
+        $('.content').append(this.tagEditView.el);
+        
+        $('.content').append('<br><br>');
+        
+       var tag = new CADENT.TagView();
+        $('.content').append(tag.el);
+        
+        $('.content').append('<br><br>');
+        
+        var projectList = new CADENT.ProjectCollection();
+        projectList.fetch({success: function(){
+            $(".content").append(new CADENT.ProjectListView({model: projectList,}).el);
+        }});
+        */
+    },
+    
+    edit: function() {
+    	console.log('ROUTER :: edit');
         
         this.headerView = new CADENT.HeaderView();
         $('.header').html(this.headerView.el);
@@ -46,9 +81,5 @@ CADENT.App = Backbone.Router.extend({
         projectList.fetch({success: function(){
             $(".content").append(new CADENT.ProjectListView({model: projectList,}).el);
         }});
-    },
-    
-    projects: function() {
-    	
     }
 });
