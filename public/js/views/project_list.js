cb39ac7c0bcce89d7965639dcc2b9d4af266f86a
drivativ <drivativ@gmail.com>
2013-01-10 21:35:11 -0800

    adding list

--- public/js/views/project_list.js
+++ public/js/views/project_list.js
@@ -12,19 +12,35 @@ CADENT.ProjectListView = Backbone.View.extend({
 		//var startPos = (this.options.page - 1) * 8;
 		//var endPos = Math.min(startPos + 8, len);
 
-		//$(this.el).html('<ul class="thumbnails"></ul>');
-		var str = 'PROJECTS<br><br>';
+		$(this.el).html('<ul class="thumbnails"></ul>');
+		//var str = 'PROJECTS<br><br>';
 		//var str = JSON.stringify(this.model, undefined, 2);
 		
         for (var i = 0; i < len; i++) {
-            str.concat(projects[i].name, '<br>');
+            //str.concat(projects[i].name, '<br>');
             
-            //$('.thumbnails', this.el).append(new WineListItemView({model: wines[i]}).render().el);
+            $('.thumbnails', this.el).append(new ProjectListItemView({model: projects[i]}).render().el);
         }
         
-		$(this.el).html(str);
-       // $(this.el).append(new Paginator({model: this.model, page: this.options.page}).render().el);
+		//$(this.el).html(str);
+		//(this.el).append(new Paginator({model: this.model, page: this.options.page}).render().el);
 
         return this;
     }
+});
+
+CADENT.ProjectListItemView = Backbone.View.extend({
+
+    tagName: "li",
+
+    initialize: function () {
+        //this.model.bind("change", this.render, this);
+        //this.model.bind("destroy", this.close, this);
+    },
+
+    render: function () {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    }
+
 });
