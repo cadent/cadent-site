0e1d1b52b47295d6acc97720e7b5533a5345cad4
Sean Samuelson <core@Seans-iMac.local>
2013-01-29 19:13:36 -0800

    ...

--- public/js/models/models.js
+++ public/js/models/models.js
@@ -14,7 +14,7 @@ CADENT.Project = Backbone.Model.extend({
         pid: "Missing_PID",
         order: 1,
         subtitle: 'Project Title',
-        url: "http://www.cadentdesign.com",
+        url: "",
         desc: "Periscopic, a Portland design agency, hired me to implement a complex data visualization application they’d designed, whose purpose was to provide multiple ways to view and sort sockeye salmon spawning data for the past 60 years. I came in as lead architect directing a small team of internal developers and developed the Flex architecture and most of the core components of the piece, including a graph able to handle and aggregate hundreds of data lines and a modeling feature using a physics-heavy exploration view of the data. I also handled the internal navigation, deep linking, and the loading of smaller Flash elements. The application went on to win an award from Communication Arts and was a finalist at the Adobe MAX Awards.",
         imgs: ['deck_1.jpg','deck_2.jpg','deck_3.jpg','deck_4.jpg','deck_5.jpg','deck_6.jpg','deck_7.jpg'],
         tech: ['JS','PHP','MySQL','HTML5','jQuery','Backbone','Node','LESS'],
